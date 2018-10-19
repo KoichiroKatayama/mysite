25e5091b0fc87c3555fc3e05dd063e2cbdf036fa
t16cs015 <t16cs015@kw02.kki.yamanashi.ac.jp>
2018-10-19 20:50:11 +0900

    全てのファイルの不要な部分を削除

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
+
 
 # Create your views here.
-from django.http import HttpResponse, response
-from django.template.context_processors import request
-from django.http import HttpResponse
-from django.template import loader
+
 from .models import Question, Choice
-from django.http import Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from django.urls import reverse
