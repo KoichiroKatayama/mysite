47592b9e077b81076d4ef9f9ebed952bc89d8068
t16cs015 <t16cs015@kw02.kki.yamanashi.ac.jp>
2018-10-19 19:57:24 +0900

    きえちゃったやつ

--- polls/urls.py
+++ polls/urls.py
@@ -8,6 +8,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     
@@ -16,5 +17,7 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
     #test
     ]
