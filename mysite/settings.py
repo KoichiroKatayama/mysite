e62cc5e0c37ad634df374a19fec45f01dc5c5cf7
t16cs015 <t16cs015@jw03.kki.yamanashi.ac.jp>
2018-10-19 15:44:23 +0900

    APIで遊んでみる終了

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'JST'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
